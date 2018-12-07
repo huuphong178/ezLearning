eb5a0af1d8f914ea1cec124403dacf7a805bb4bb
donguyen131197 <do.nguyen131197@gmail.com>
2018-12-07 12:19:21 +0700

    làm cái carousel nhưng ngu quá nên bị lỗi :'(

--- ezlearning-frontend/UI/res/js/tile.js
+++ ezlearning-frontend/UI/res/js/tile.js
@@ -1,8 +1,29 @@
 $(".tile-course").hover(
-    function(){
+    function () {
         $(this).parent("div").find(".tile-preview").show();
     },
-    function(){
+    function () {
         $(this).parent("div").find(".tile-preview").hide();
     }
 )
+
+function showSlides(curId) {
+    var nextId;
+    nextId = curId + 1;
+    if (nextId > 3) nextId = 1;
+    var nextObj = $("#v" + nextId);
+    var curObj = $("#v" + curId);
+    nextObj.fadeIn();   
+    curObj.fadeOut();
+    setTimeout(showSlides(nextId), 3000);
+}
+
+$(document).ready(function () {
+
+    $("#v2").hide();
+    $("#v3").hide();
+
+    setTimeout(showSlides(1), 3000);
+
+
+})
