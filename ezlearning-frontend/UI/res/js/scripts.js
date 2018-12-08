0a7ee44ef4c415a68015a4992be89afe4814cfab
donguyen131197 <do.nguyen131197@gmail.com>
2018-12-08 19:10:20 +0700

    Menu dropdown Danh mục

--- ezlearning-frontend/UI/res/js/scripts.js
+++ ezlearning-frontend/UI/res/js/scripts.js
@@ -8,14 +8,48 @@ $(document).ready(function () {
     });
 })
 
-$("#fteachers-right").click(function(){
-    $("#fteachers-list").animate({marginLeft: "-102%"}, 600);
+$("#fteachers-right").click(function () {
+    $("#fteachers-list").animate({
+        marginLeft: "-102%"
+    }, 600);
     $("#fteachers-right").fadeOut("fast");
     $("#fteachers-left").fadeIn("fast");
 })
 
-$("#fteachers-left").click(function(){
-    $("#fteachers-list").animate({marginLeft: "0%"}, 600);
+$("#fteachers-left").click(function () {
+    $("#fteachers-list").animate({
+        marginLeft: "0%"
+    }, 600);
     $("#fteachers-right").fadeIn("fast");
     $("#fteachers-left").fadeOut("fast");
 })
+
+$("#nav-cats").hover(
+    function () {
+        $(this).parent().find("#dropdown-cats").fadeIn("fast");
+    }
+)
+
+$("#nav-cats").parent().find("#dropdown-cats").hover(
+    function () {
+        $(this).fadeIn("fast");
+    },
+    function () {
+        $(this).fadeOut("fast");
+    }
+)
+
+$("#nav-cats-banner").hover(
+    function () {
+        $(this).parent().find("#dropdown-cats").fadeIn("fast");
+    }
+)
+
+$("#nav-cats-banner").parent().find("#dropdown-cats").hover(
+    function () {
+        $(this).fadeIn("fast");
+    },
+    function () {
+        $(this).fadeOut("fast");
+    }
+)
