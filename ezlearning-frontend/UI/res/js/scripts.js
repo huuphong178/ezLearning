562f805bfd7232bb7c0912c9cb70304552201179
donguyen131197 <do.nguyen131197@gmail.com>
2018-12-09 02:57:07 +0700

    Form sign-up sign-in

--- ezlearning-frontend/UI/res/js/scripts.js
+++ ezlearning-frontend/UI/res/js/scripts.js
@@ -113,3 +113,65 @@ $("#nav-profile-t").parent().find("#dropdown-profile").hover(
         $(this).fadeOut("fast");
     }
 )
+
+function signUpModal() {
+    $("#sign-up-modal").show();
+    $("#sign-in-modal").hide();
+    $("#sign-in-tab").removeClass("active");
+    $("#sign-up-tab").addClass("active");
+}
+
+function signInModal() {
+    $("#sign-up-modal").hide();
+    $("#sign-in-modal").show();
+    $("#sign-up-tab").removeClass("active");
+    $("#sign-in-tab").addClass("active");
+}
+
+$("#sign-up-tab").click(function () {
+    signUpModal();
+})
+
+$("#sign-in-tab").click(function () {
+    signInModal();
+})
+
+function suWarning(msg) {
+    $("#su-warn").css("opacity","0");
+    setTimeout(function () {
+        $("#su-warn").animate({opacity: "1"},200);
+    }, 50);
+    $("#su-warn").text(msg);
+}
+
+function siWarning(msg) {
+    $("#si-warn").css("opacity","0");
+    setTimeout(function () {
+        $("#si-warn").animate({opacity: "1"},200);
+    }, 50);
+    $("#si-warn").text(msg);
+}
+
+$("#sign-up-submit").click(function(){
+    $("#su-warn").show();
+    suWarning("Test 1");
+})
+
+$("#sign-in-submit").click(function(){
+    $("#si-warn").show();
+    siWarning("Test 2");
+})
+
+$(".sign-up-btn").click(function(){
+    $("#su-si-modal").fadeIn("fast");
+    signUpModal();
+})
+
+$(".sign-in-btn").click(function(){
+    $("#su-si-modal").fadeIn("fast");
+    signInModal();
+})
+
+$("#su-si-close").click(function(){
+    $("#su-si-modal").fadeOut("fast");
+})
