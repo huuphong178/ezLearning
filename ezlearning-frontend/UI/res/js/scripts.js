29f381c285aa5f27697193c98f8cc155b63c44f8
donguyen131197 <37667792+donguyen131197@users.noreply.github.com>
2018-12-11 07:01:44 +0700

    1 phần trang detail

--- ezlearning-frontend/UI/res/js/scripts.js
+++ ezlearning-frontend/UI/res/js/scripts.js
@@ -1,13 +1,3 @@
-$(document).ready(function () {
-    $(document).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $(".navbar").slideDown(200);
-        } else {
-            $('.navbar').slideUp(200);
-        }
-    });
-})
-
 $("#fteachers-right").click(function () {
     $("#fteachers-list").animate({
         marginLeft: "-102%"
