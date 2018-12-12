d0908e01f2100deb61c51ad7992e31bcd377cbbf
donguyen131197 <37667792+donguyen131197@users.noreply.github.com>
2018-12-12 14:37:48 +0700

    Update course-detail

--- ezlearning-frontend/UI/res/js/course-detail.js
+++ ezlearning-frontend/UI/res/js/course-detail.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-    $('html,body').animate({scrollTop: 1},'fast');
+    $('html,body').animate({ scrollTop: 1 }, 'fast');
 
     function goToByScroll(id) {
         id = id.replace("-tab", "");
         $('html,body').animate({
-                scrollTop: ($("#" + id).offset().top - 130)
-            },
+            scrollTop: ($("#" + id).offset().top - 130)
+        },
             'slow');
     }
 
@@ -25,7 +25,7 @@ $(document).ready(function () {
         var teacherTop = $("#teacher").offset().top - 131;
         var ratingTop = $("#rating").offset().top - 131;
 
-        if (($(this).scrollTop() >= infoTop) && ($(this).scrollTop() <= lectureTop)){
+        if (($(this).scrollTop() >= infoTop) && ($(this).scrollTop() <= lectureTop)) {
             if ($("#info-tab").hasClass("tab-disabled")) {
                 $(".tab").removeClass("tab-enabled");
                 $(".tab").addClass("tab-disabled");
@@ -34,7 +34,7 @@ $(document).ready(function () {
             }
         }
 
-        if (($(this).scrollTop() >= lectureTop) && ($(this).scrollTop() <= teacherTop)){
+        if (($(this).scrollTop() >= lectureTop) && ($(this).scrollTop() <= teacherTop)) {
             if ($("#lecture-tab").hasClass("tab-disabled")) {
                 $(".tab").removeClass("tab-enabled");
                 $(".tab").addClass("tab-disabled");
@@ -43,7 +43,7 @@ $(document).ready(function () {
             }
         }
 
-        if (($(this).scrollTop() >= teacherTop) && ($(this).scrollTop() <= ratingTop)){
+        if (($(this).scrollTop() >= teacherTop) && ($(this).scrollTop() <= ratingTop)) {
             if ($("#teacher-tab").hasClass("tab-disabled")) {
                 $(".tab").removeClass("tab-enabled");
                 $(".tab").addClass("tab-disabled");
@@ -52,7 +52,7 @@ $(document).ready(function () {
             }
         }
 
-        if (($(this).scrollTop() >= ratingTop)){
+        if (($(this).scrollTop() >= ratingTop)) {
             if ($("#rating-tab").hasClass("tab-disabled")) {
                 $(".tab").removeClass("tab-enabled");
                 $(".tab").addClass("tab-disabled");
@@ -84,6 +84,54 @@ $(document).ready(function () {
             $("#payment").css("top", stopPos + 60);
         }
 
-
     });
+
+    var starChoose = false;
+    var lastChoose;
+
+    function rateChange(e) {
+        var thisId = e.attr("id");
+        curId = + thisId.replace("rate-", "");
+        for (i = 1; i <= curId; i++) {
+            changeId = "rate-" + i;
+            $("#" + changeId).attr("src", "res/icons/ratings/rate-full.png");
+        }
+        for (i > curId; i <= 5; i++) {
+            changeId = "rate-" + i;
+            $("#" + changeId).attr("src", "res/icons/ratings/rate-outline.png");
+        }
+    }
+
+    $(".rate-act").mouseenter(function () {
+        rateChange($(this));
+    })
+
+    $(".rate-star").mouseleave(function () {
+        if (starChoose == false) {
+            for (i = 1; i <= 5; i++) {
+                changeId = "rate-" + i;
+                $("#" + changeId).attr("src", "res/icons/ratings/rate-outline.png");
+            }
+        }
+        else {
+            rateChange(lastChoose);
+        }
+    })
+
+    $(".rate-act").click(function () {
+        rateChange($(this));
+        lastChoose = $(this);
+        starChoose = true;
+    })
+
+    $("#rate-submit").click(function(){
+        $(".comment-act").hide();
+        $(".re-rate").show();
+    })
+
+    $(".re-rate > a").click(function(){
+        $(".comment-act").show();
+        $(".re-rate").hide();
+    })
+
 })
