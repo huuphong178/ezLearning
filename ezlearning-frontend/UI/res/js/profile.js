ed71b4c9c37551cc36566d00201fe363d98668f4
donguyen131197 <37667792+donguyen131197@users.noreply.github.com>
2018-12-14 02:56:52 +0700

    Hoàn tất profile của student

---
+++ ezlearning-frontend/UI/res/js/profile.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".tab").click(function () {
+        id = $(this).attr("id").replace("-tab", "");
+        $(".tab").removeClass("tab-enabled");
+        $(".tab").addClass("tab-disabled");
+        $(this).addClass("tab-enabled");
+        $(this).removeClass("tab-disabled");
+        $(".tab-content").hide();
+        $("#" + id).show();
+    })
+})
